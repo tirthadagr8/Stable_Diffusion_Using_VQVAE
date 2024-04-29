c927ad2f316e07a1ae5da7061e3224147defea92
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2024-04-29 20:53:21 +0530

    Update UNET.py

--- UNET.py
+++ UNET.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sd.parameters import hp
 
+
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels,temb_dim,layers,self_attention=False,cross_attention=False,context_dim=None):
         super(ResidualBlock, self).__init__()
@@ -100,6 +101,12 @@ class UNET(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv_in=nn.Conv2d(hp.in_channels,hp.down_blocks[0],kernel_size=3,stride=1,padding=1)
+        self.t_proj=nn.Sequential(
+            nn.Linear(hp.temb_dim,hp.temb_dim),
+            nn.SiLU(),
+            nn.Linear(hp.temb_dim,hp.temb_dim)
+        )
+
         self.downblock=nn.ModuleList([])
         for i in range(len(hp.down_blocks)-1):
             self.downblock.append(
@@ -113,40 +120,50 @@ class UNET(nn.Module):
                 ResidualBlock(hp.mid_blocks[i],hp.mid_blocks[i+1],hp.temb_dim,hp.num_layers,True,True,hp.text_embed_dim)
             )
         
+        # 2 1 0
         self.upblock=nn.ModuleList([])
-        for i in range(len(hp.down_blocks)-1):
+        for i in reversed(range(len(hp.down_blocks) - 1)):
+            self.upblock.append(nn.ConvTranspose2d(hp.down_blocks[i],hp.down_blocks[i],kernel_size=4,stride=2,padding=1))
             self.upblock.append(
-                ResidualBlock(hp.up_blocks[i],hp.up_blocks[i+1],hp.temb_dim,hp.num_layers,True,True,hp.text_embed_dim)
+                ResidualBlock(hp.down_blocks[i]*2,hp.down_blocks[i-1] if i!=0 else hp.conv_out_channels,hp.temb_dim,hp.num_layers,True,True,hp.text_embed_dim)
             )
-            self.upblock.append(nn.ConvTranspose2d(hp.up_blocks[i+1],hp.up_blocks[i+1],kernel_size=4,stride=2,padding=1))
             
-        self.norm_out=nn.GroupNorm(hp.norm_channels,hp.up_blocks[-1])
-        self.conv_out=nn.Conv2d(hp.up_blocks[-1],3,kernel_size=3,stride=1,padding=1)
+            
+        self.norm_out=nn.GroupNorm(hp.norm_channels,hp.conv_out_channels)
+        self.silu=nn.SiLU()
+        self.conv_out=nn.Conv2d(hp.conv_out_channels,3,kernel_size=3,stride=1,padding=1)
         
 
     def forward(self,x,t_emb,context):
         x=self.conv_in(x)
+        t_emb=self.t_proj(t_emb)
+        down_outs = []
         for layer in self.downblock:
             if isinstance(layer,ResidualBlock):
+                down_outs.append(x)
                 x=layer(x,t_emb,context)
             else:
                 x=layer(x)
+                
         for layer in self.midblock:
             if isinstance(layer,ResidualBlock):
                 x=layer(x,t_emb,context)
             else:
                 x=layer(x)
+               
         for layer in self.upblock:
             if isinstance(layer,ResidualBlock):
                 x=layer(x,t_emb,context)
             else:
                 x=layer(x)
+                out_down=down_outs.pop()
+                x = torch.cat([x, out_down], dim=1)
             
-        x=F.silu(self.norm_out(x))
+            
+        x=self.silu(self.norm_out(x))
         x=self.conv_out(x)
         return x
         
-
-
-            
+diffusion=UNET()
+        
 
